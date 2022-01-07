6c08032be05de1790ec7d994caa4ca167b2999cf
dishaa-patel <disha.softsages@gmail.com>
2022-01-07 18:50:49 +0530

    textutils1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 function App() {
   const[mode,setMode] = useState('dark');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=> {
+    setAlert({
+      msg:message,
+      type: type
+    })
+
+  }
 
   const toggleMode = () =>{
     if(mode==='light'){
       setMode('dark');
+      setAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
+      setAlert("Light mode has been enabled","success");
     }
   }
   return (
     <>
       <NavBar title = "TextUtils" aboutText="About" mode= {mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert} />
       <About />
       <TextForm heading = "Enter the text below" />
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -44,7 +44,7 @@ export default function TextForm(props) {
                 <p>{text.split(" ").length} words and {text.length} characters </p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter something to preview it here"}</p>
             </div>
         </div>
     )
